9383a18b8ce920259aae43064e0d8b206625e733
Mike <mike1072@gmail.com>
2016-01-30 17:08:11 -0800

    add spell parsing

--- Assets/Spells/SpellDescriptor.cs
+++ Assets/Spells/SpellDescriptor.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class SpellDescriptor
 {
     public string Name { get; private set; }
 
-    public SpellDescriptor(string name)
+    Dictionary<string, float> opinions;
+
+    public SpellDescriptor(string name, Dictionary<string, float> opinions)
     {
         this.Name = name;
+        this.opinions = opinions;
+    }
+
+    public float GetOpinion(Region region)
+    {
+        if (!opinions.ContainsKey(region.InternalName))
+            throw new ArgumentException("No opinion data available for region " + region.InternalName + " (" + region.DisplayedName + ")");
+
+        return opinions[region.InternalName];
     }
 }
--- Assets/Spells/SpellObject.cs
+++ Assets/Spells/SpellObject.cs
@@ -1,12 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class SpellObject
 {
     public string Name { get; private set; }
 
-    public SpellObject(string name)
+    Dictionary<string, float> opinions;
+
+    public SpellObject(string name, Dictionary<string, float> opinions)
     {
         this.Name = name;
+        this.opinions = opinions;
     }
+
+    public float GetOpinion(Region region)
+    {
+        if (!opinions.ContainsKey(region.InternalName))
+            throw new ArgumentException("No opinion data available for region " + region.InternalName + " (" + region.DisplayedName + ")");
+
+        return opinions[region.InternalName];
+    }
+
+    //public string GetOpinionStrings()
+    //{
+    //    return string.Join(" ", opinions.Values.Select(u => u.ToString("f0")).ToArray());
+    //}
 }
